abe3c52da5a24f8a05f713ba309ab709decfaa00
billytrinh <quanghoa.trinh@gmail.com>
2020-06-15 10:26:36 +0700

    update heroku

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'pgsql',//env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            //'url' => env('DATABASE_URL'),
+            'host' => 'ec2-52-44-55-63.compute-1.amazonaws.com',//env('DB_HOST', '127.0.0.1'),
+            'port' => '5432',//env('DB_PORT', '5432'),
+            'database' => 'dbf6nvp1ejkr93',//env('DB_DATABASE', 'forge'),
+            'username' => 'ccrcixbxucjxjb',//env('DB_USERNAME', 'forge'),
+            'password' => '8aac08ad3cbf002b5937a2224b9a6c28dd743c8a2c8948d37ff25f6e744d3022',//env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
